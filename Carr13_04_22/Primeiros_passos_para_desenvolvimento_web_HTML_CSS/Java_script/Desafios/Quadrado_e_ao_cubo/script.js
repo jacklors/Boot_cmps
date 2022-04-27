717d4b9e3fdd44fc7df7a2d605616af694dd2eca
Jackson Loredo <jackson.loredo.main@gmail.com>
2022-04-27 08:41:32 -0300

    Adicionando Algumas Atividades

--- Carr13_04_22/Primeiros_passos_para_desenvolvimento_web_HTML_CSS/Java_script/Desafios/Quadrado_e_ao_cubo/script.js
+++ Carr13_04_22/Primeiros_passos_para_desenvolvimento_web_HTML_CSS/Java_script/Desafios/Quadrado_e_ao_cubo/script.js
@@ -1,27 +1,3 @@
-/*
-Desafio
-
-Você terá o desafio de escrever um programa que leia um valor inteiro N (1 < N < 1000). Este N é a quantidade de linhas de saída que serão apresentadas na execução do programa.
-Entrada
-
-O arquivo de entrada contém um número inteiro positivo N.
-Saída
-
-Imprima a saída conforme o exemplo fornecido.
- 
-Exemplo de Entrada 	Exemplo de Saída
-
-5
-	
-
-1 1 1
-2 4 8
-3 9 27
-4 16 64
-5 25 125
- */
-
-
 // a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados.
 // Abaixo segue um exemplo de código que você pode ou não utilizar
 
